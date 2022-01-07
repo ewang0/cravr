609734d35862ef684384e0a876aba24ec92c44d0
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-07 18:50:09 -0500

    Add image grid instructions

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Card from "./Card";
+import "./ImageGrid.css"
 
 const ImageGrid = () => {
   return(
     <section className="image-grid-section">
+      <aside className="questionnaire-info">
+        <img className="bulb-icon" src="fruit.jpg" alt="lightbulb-icon"></img>
+        <div className="questionnaire-info-text">
+          <h2>Recommendations</h2>
+          <p>Your food recommendations will show up here. We’ve generated some random foods to get you started.</p>
+        </div>
+      </aside>
+    <div className="card-container">
       <Card />
       <Card />
       <Card />
+    </div>
+
     </section>
   )
 }
