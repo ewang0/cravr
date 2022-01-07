4b6fc6a46afd8fb0f7d60bc31e9c9b4c41e0cb07
Tony Hsu <tdh425@gmail.com>
2022-01-07 16:31:43 -0700

    Create sample data

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,34 @@ import './App.css';
 import Form from './components/Form';
 import ImageGrid from './components/ImageGrid';
 import Nav from './components/Nav'
+import sampleData from './sampleData';
 
 const App: React.FC = () => {
 
-  const [recipes, setRecipes] = useState([])
-  
-  useEffect(() => {
-    const fetchData = async() => {
-      const res = await fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=dd5ac6591f404c4d9a7ea8475237d2d7&cuisine=italian&intolerances=egg&diet=vegetarian')
-      const resJson = await res.json()
-      setRecipes(resJson)
-    }
-    fetchData()
-  }, [setRecipes])
+  const [recipes, setRecipes] = useState(sampleData)
 
-  console.log('recipes', recipes )
+  
+  // useEffect(() => {
+  //   setRecipes(sampleData)
+  //   // const fetchData = async() => {
+  //   //   const res = await fetch(url)
+  //   //   const resJson = await res.json()
+  //   //   setRecipes(resJson)
+  //   // }
+  //   // fetchData()
+  // }, [setRecipes])
+
+
+
+  // console.log('recipes', recipes )
 
   
   return (
-    //when we access data, we need recipes.results
-    //this gives us an array of objects 
-    //{ id: numbers, image, imageType, title :rest are strings}
     <main className="App">
       <Nav />
       <section className="main-container">
         <Form />
-        <ImageGrid />
+        <ImageGrid recipes={recipes} />
       </section>
     </main>
   );
---
+++ src/sampleData.js
@@ -0,0 +1,29 @@
+const sampleData = [
+  {
+    id: 654928,
+    title: "Pasta with Italian Sausage",
+    image: "https://spoonacular.com/recipeImages/654928-312x231.jpg"
+  },
+  {
+    id: 654835,
+    title: "Pasta e Fagioli (Pasta and Beans)",
+    image: "https://spoonacular.com/recipeImages/654835-312x231.jpg"
+  },
+  {
+    id: 654935,
+    title: "Pasta with Peas and Italian Sausage",
+    image: "https://spoonacular.com/recipeImages/654935-312x231.jpg"
+  },
+  {
+    id: 648183,
+    title: "Italian Panna Cotta",
+    image: "https://spoonacular.com/recipeImages/648183-312x231.jpg"
+  },
+  {
+    id: 648247,
+    title: "Italian Seafood Stew",
+    image: "https://spoonacular.com/recipeImages/648247-312x231.jpg"
+  }
+]
+
+export default sampleData
