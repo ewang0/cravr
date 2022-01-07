267587bdf0b68a611498b73fb6a4c1bf9e5a4832
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-07 18:52:44 -0500

    Add flex to main container

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,21 @@ import Form from './components/Form';
 import ImageGrid from './components/ImageGrid';
 import Nav from './components/Nav'
 
-const App = () => {
+const App: React.FC = () => {
+
+  // const [recipes, setRecipes] = useState([])
+  
+  // useEffect(() => {
+  //   const fetchData = async() => {
+  //     const res = await fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=dd5ac6591f404c4d9a7ea8475237d2d7&cuisine=italian&intolerances=egg&diet=vegetarian')
+  //     const resJson = await res.json()
+  //     setRecipes(resJson)
+  //   }
+  //   fetchData()
+  // }, [setRecipes])
+
+  console.log('recipes', recipes )
+
   return (
     <main className="App">
       <Nav />
