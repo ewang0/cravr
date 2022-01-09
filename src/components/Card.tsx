56aae77d20c91da02ad4b266cbe6a6d00f9ac8e5
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-09 15:53:22 -0500

    Add see details link to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,8 +5,11 @@ import { Recipe } from './Types'
 const Card: React.FC<Recipe> =(recipe) => {
   return(
     <div className="card">
-      <img src={recipe.image}></img>
-      <p>{recipe.title}</p>
+      <img className="food-img"src={recipe.image}></img>
+      <div className="card-text">
+        <p>{recipe.title}</p>
+        <a className="see-details"href="">See details<img src="arrow.svg"></img></a>
+      </div>
     </div>
   )
 }
