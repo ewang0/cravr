e11d2b497eb3c157bdc58ef830958e4a720b3133
jose Tovar <jose.tovar102397@gmail.com>
2022-01-09 15:52:06 -0700

    Add endpoints to URL

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,22 @@ import sampleData from './sampleData';
 const App: React.FC = () => {
 
   const [recipes, setRecipes] = useState(sampleData)
-  
+  const [endPoint, setEndPoint] = useState('https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=dd5ac6591f404c4d9a7ea8475237d2d7&cuisine=italian&intolerances=egg&diet=vegetarian');
+  // 'https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=dd5ac6591f404c4d9a7ea8475237d2d7&cuisine=italian&intolerances=egg&diet=vegetarian'
   // useEffect(() => {
   //   const fetchData = async() => {
-  //     const res = await fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=dd5ac6591f404c4d9a7ea8475237d2d7&cuisine=italian&intolerances=egg&diet=vegetarian')
+  //     const res = await fetch(endPoint)
   //     const resJson = await res.json()
+  //     console.log("this is the response>>", resJson.results)
   //     setRecipes(resJson.results)
   //   }
-  //   fetchData()
-  // }, [setRecipes])
-  const submitSearch = (event: any, stringArr?: string[]) => {
+  //   fetchData();
+  // }, [endPoint]);
+
+  const submitSearch = (event: any, type?: string) => {
     event.preventDefault();
     console.log('hello')
+    setEndPoint(`https://api.spoonacular.com/recipes/complexSearch?type=${type}&apiKey=dd5ac6591f404c4d9a7ea8475237d2d7`);
   }
 
   return (
