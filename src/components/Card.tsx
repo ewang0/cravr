91f7668b2dbae5cb8ee518e97b2e5420303d7fd5
jose Tovar <jose.tovar102397@gmail.com>
2022-01-11 09:14:38 -0700

    Add navigation link to details page in card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import './Card.css';
+import Details from "./Details";
 import { Recipe } from './Types'
+import { Routes, Route, NavLink } from "react-router-dom";
 
-const Card: React.FC<Recipe> =(recipe) => {
+const Card: React.FC<Recipe> =({ id, title, image }) => {
   return(
     <div className="card">
-      <img className="food-img"src={recipe.image}></img>
+      <img className="food-img" src={image} alt="Food IMG"></img>
       <div className="card-text">
-        <p>{recipe.title}</p>
-        <a className="see-details"href="">See details<img src="arrow.svg"></img></a>
+        <p>{title}</p>
+        <NavLink to={`/details/${id}`} className="see-details">See Details</NavLink>
+        {/* <Routes >
+          <Route path="/details" element={<Details />}/>
+        </Routes> */}
+        {/* <a className="see-details"href="">See details<img src="arrow.svg" alt="Forward Arrow"></img></a> */}
       </div>
     </div>
   )
