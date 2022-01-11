873d2dc192b5b5a92c6fbd5c30331b44a9dbaa7f
jose Tovar <jose.tovar102397@gmail.com>
2022-01-11 15:48:51 -0700

    Uncomment use effect hook

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,20 +11,19 @@ const Home: React.FC = () => {
   const [dietRestrictionEndPoint, setDietRestrictionEndPoint] = useState('');
   const [intoleranceEndPoint, setIntoleranceEndPoint] = useState('');
 
-  // useEffect(() => {
-  //   const fetchData = async() => {
-  //     const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?${cuisineTypeEndPoint}${intoleranceEndPoint}${dietRestrictionEndPoint}${typeEndPoint}&apiKey=dd5ac6591f404c4d9a7ea8475237d2d7`)
-  //     const resJson = await res.json()
-  //     .catch(error => console.log(error));
-  //     // !resJson.length ? 
-  //     setRecipes(resJson.results)
-  //   }
-  //   fetchData();
-  // }, [typeEndPoint, cuisineTypeEndPoint, dietRestrictionEndPoint, intoleranceEndPoint]);
+  useEffect(() => {
+    const fetchData = async() => {
+      const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?${cuisineTypeEndPoint}${intoleranceEndPoint}${dietRestrictionEndPoint}${typeEndPoint}&apiKey=dd5ac6591f404c4d9a7ea8475237d2d7`)
+      const resJson = await res.json()
+      .catch(error => console.log(error));
+      // !resJson.length ? 
+      setRecipes(resJson.results)
+    }
+    fetchData();
+  }, [typeEndPoint, cuisineTypeEndPoint, dietRestrictionEndPoint, intoleranceEndPoint]);
 
   const submitSearch = (event: any, type?: string, cuisineTypes?: string[], dietRestrictions?: string[], intolerances?: string[]) => {
     event.preventDefault();
-    console.log('hello');
     if (cuisineTypes) {
       setCuisineTypeEndPoint(`cuisine=${cuisineTypes.join(",")}`)
     }
