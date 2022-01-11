b2cf09346b70a3a455fd47ff6b46c02daeea1281
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-11 17:43:08 -0500

    Style details page

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -6,8 +6,9 @@ const Details: React.FC = () => {
 
   return (
     <section className="recipe-details">
-      <Link to="/">back</Link>
+      
       <div className="recipe-details-content">
+        <Link className="back-button" to="/"><img className="left-arrow-icon" src="arrow.svg"></img>Back</Link>
         <div className="content-left">
           <div className="image-description">
             {/* <img></img> */}
@@ -26,10 +27,7 @@ const Details: React.FC = () => {
             <h2>Description</h2>
             <p>Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs might be a good recipe to expand your main course repertoire. One portion of this dish contains approximately <b>19g of protein </b>,  <b>20g of fat </b>, and a total of  <b>584 calories </b>. For  <b>$1.63 per serving </b>, this recipe  <b>covers 23% </b> of your daily requirements of vitamins and minerals. This recipe serves 2. It is brought to you by fullbellysisters.blogspot.com. 209 people were glad they tried this recipe. A mixture of scallions, salt and pepper, white wine, and a handful of other ingredients are all it takes to make this recipe so scrumptious. From preparation to the plate, this recipe takes approximately  <b>45 minutes </b>.</p>
           </div>
-          <div>
-            <h2>Link to Recipe</h2>
-            <p>https://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html</p>
-          </div>
+          <a href=""><button className="view-recipe-button">View Recipe</button></a>
         </div>
       </div>
     </section>
