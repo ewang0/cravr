d584bc064a302c98b122691afd40202881d308fe
jose Tovar <jose.tovar102397@gmail.com>
2022-01-12 10:07:24 -0700

    ffix indentation in nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,19 +3,19 @@ import './Nav.css';
 import { NavLink } from 'react-router-dom'
 
 const Nav: React.FC = () => {
-    return (
-        <nav>
-            <div className="nav-items left">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-            </div>
-            <h1>Cravr</h1>
-            <div className="nav-items right">
-                <NavLink to="/">Favorites</NavLink>
-                <NavLink to="/">Something</NavLink>
-            </div>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className="nav-items left">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </div>
+      <h1>Cravr</h1>
+      <div className="nav-items right">
+        <NavLink to="/">Favorites</NavLink>
+        <NavLink to="/">Something</NavLink>
+      </div>
+    </nav>
+  )
 }
 
 export default Nav;
